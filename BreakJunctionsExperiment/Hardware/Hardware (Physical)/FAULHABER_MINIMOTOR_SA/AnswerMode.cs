341896c40bda13aa30ea83af0316dc35e25dd18d
zigorrom <https://zigorrom@zigorrom.git.cloudforge.com/originprojectcreator.git>
2014-08-08 11:23:45 +0200

    Functions for motor provided

---
+++ BreakJunctionsExperiment/Hardware/Hardware (Physical)/FAULHABER_MINIMOTOR_SA/AnswerMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BreakJunctions.Hardware.Hardware__Physical_.FAULHABER_MINIMOTOR_SA
+{
+    public enum AnswerMode
+    {
+        NoAsynchronousResponses=0,
+        AllowAsynchronousResponses,
+        AllCommandsWithConfirmationAndAsyncResponses,
+        SentCommandsAreReturned
+    }
+}
