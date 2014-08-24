48845b400679607bd04219d8c191dcde4ce40ea7
Handziuk, Volodymyr <gandziuk2009@gmail.com>
2014-08-24 15:16:53 +0200

    Motor interface developing

---
+++ BreakJunctionsExperiment/User Interfaces/Hardware Interfaces/Hardware (Physical)/FaulhaberMinimotor/Faulhaber_2036_U012V.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BreakJunctions
+{
+    /// <summary>
+    /// Interaction logic for Faulhaber_2036_U012V.xaml
+    /// </summary>
+    public partial class Faulhaber_2036_U012V : Window
+    {
+        public Faulhaber_2036_U012V()
+        {
+            this.DataContext = new MVVM_Faulhaber_2036_U012V();
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ BreakJunctionsExperiment/User Interfaces/Hardware Interfaces/Hardware (Physical)/FaulhaberMinimotor/MVVM_Faulhaber_2036_U012V.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+
+namespace BreakJunctions
+{
+    class MVVM_Faulhaber_2036_U012V : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string PropertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        #endregion
+
+        #region COM port
+
+        private string[] _AvaliablePorts = SerialPort.GetPortNames();
+        public string[] AvaliablePorts
+        {
+            get { return _AvaliablePorts; }
+            set
+            {
+                _AvaliablePorts = value;
+                OnPropertyChanged("AvaliablePorts");
+            }
+        }
+
+        #endregion
+    }
+}
