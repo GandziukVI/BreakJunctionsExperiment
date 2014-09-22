f4c3ba2ddc9b02df84c3e90a5280d6b5dbf2187a
Handziuk, Volodymyr <gandziuk2009@gmail.com>
2014-09-22 16:53:57 +0200

    Form for controlling real time time trace measurements established

--- BreakJunctionsExperiment/User Interfaces/Application Controls/MainWindow Controls/ModelViews/RealTimeTimeTraceMeasurementSettingsDataModel.cs
+++ BreakJunctionsExperiment/User Interfaces/Application Controls/MainWindow Controls/ModelViews/RealTimeTimeTraceMeasurementSettingsDataModel.cs
@@ -82,16 +82,94 @@ namespace BreakJunctions
 
         #endregion
 
-        private double _RealTimeTimeTraceMeasurementAppliedVoltage = 0.02;
-        public double RealTimeTimeTraceMeasurementAppliedVoltage
+        #region Use channels
+
+        private bool _IsChannel_01_Checked = true;
+        public bool IsChannel_01_Checked
         {
-            get { return _RealTimeTimeTraceMeasurementAppliedVoltage; }
+            get { return _IsChannel_01_Checked; }
             set
             {
-                _RealTimeTimeTraceMeasurementAppliedVoltage = value;
-                OnPropertyChanged("RealTimeTimeTraceMeasurementAppliedVoltage");
+                _IsChannel_01_Checked = value;
+                OnPropertyChanged("IsChannel_01_Checked");
             }
         }
+
+        private bool _IsChannel_02_Checked = true;
+        public bool IsChannel_02_Checked
+        {
+            get { return _IsChannel_02_Checked; }
+            set
+            {
+                _IsChannel_02_Checked = value;
+                OnPropertyChanged("IsChannel_02_Checked");
+            }
+        }
+
+        private bool _IsChannel_03_Checked = true;
+        public bool IsChannel_03_Checked
+        {
+            get { return _IsChannel_03_Checked; }
+            set
+            {
+                _IsChannel_03_Checked = value;
+                OnPropertyChanged("IsChannel_03_Checked");
+            }
+        }
+
+        private bool _IsChannel_04_Checked = true;
+        public bool IsChannel_04_Checked
+        {
+            get { return _IsChannel_04_Checked; }
+            set
+            {
+                _IsChannel_04_Checked = value;
+                OnPropertyChanged("IsChannel_04_Checked");
+            }
+        }
+
+        #endregion
+
+        #region Amplification coefficient
+
+        private double _AmplificationCoefficient = 1000000.0;
+        public double AmplificationCoefficient
+        {
+            get { return _AmplificationCoefficient; }
+            set
+            {
+                _AmplificationCoefficient = value;
+                OnPropertyChanged("AmplificationCoefficient");
+            }
+        }
+
+        #endregion
+
+        #region Measured resistance
+
+        private double _Resistance_1st_Sample;
+        public double Resistance_1st_Sample
+        {
+            get { return _Resistance_1st_Sample; }
+            set
+            {
+                _Resistance_1st_Sample = value;
+                OnPropertyChanged("Resistance_1st_Sample");
+            }
+        }
+
+        private double _Resistance_2nd_Sample;
+        public double Resistance_2nd_Sample
+        {
+            get { return _Resistance_2nd_Sample; }
+            set
+            {
+                _Resistance_2nd_Sample = value;
+                OnPropertyChanged("Resistance_2nd_Sample");
+            }
+        }
+
+        #endregion
 
         #endregion
     }
