6667293b3a8b197ecdb0a469b6dbcefe288157ee
GandziukVI <gandziuk2009@gmail.com>
2015-01-09 15:36:43 +0100

    Interface for noise measurement created

---
+++ BreakJunctionsExperiment/User Interfaces/Application Controls/MainWindow Controls/ModelViews/NoiseMeasurementDataModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakJunctions
+{
+    public class NoiseMeasurementDataModel : INotifyPropertyChanged
+    {
+        #region InotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string PropertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        #endregion
+
+        #region NoiseMeasurementDataModel inplementation
+
+        private int _NumberOfSpectra = 100;
+        public int MunberOfSpectra
+        {
+            get { return _NumberOfSpectra; }
+            set
+            {
+                _NumberOfSpectra = value;
+                OnPropertyChanged("MunberOfSpectra");
+            }
+        }
+
+        private int _DisplayUpdateNumber = 5;
+        public int DisplayUpdateNumber
+        {
+            get { return _DisplayUpdateNumber; }
+            set
+            {
+                _DisplayUpdateNumber = value;
+                OnPropertyChanged("DisplayUpdateNumber");
+            }
+        }
+
+        private string _Comment = "";
+        public string Comment
+        {
+            get { return _Comment; }
+            set
+            {
+                _Comment = value;
+                OnPropertyChanged("Comment");
+            }
+        }
+
+        #endregion
+    }
+}
