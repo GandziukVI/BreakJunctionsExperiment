3a4564bc2cabcddcbcfbea607aa348dae6c99bd4
GandziukVI <gandziuk2009@gmail.com>
2015-02-11 18:49:48 +0100

    SourceSetupPage programming started

--- Keithley_4200/Pages/SourceSetupPage.cs
+++ Keithley_4200/Pages/SourceSetupPage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Devices;
+using Keithley_4200.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,47 @@ using System.Threading.Tasks;
 
 namespace Keithley_4200.Pages
 {
-    class SourceSetupPage
+    public class SourceSetupPage
     {
+        #region SourceSetupPage settings
+
+        private IExperimentalDevice _TheDevice;
+
+        private bool _IsSourceSetupPageSelected = false;
+        public bool IsSourceSetupPageSelected { get { return _IsSourceSetupPageSelected; } }
+
+        #endregion
+
+        #region Constructor
+
+        public SourceSetupPage(ref IExperimentalDevice __TheDevice)
+        {
+            _TheDevice = __TheDevice;
+
+            AllEventsHandler.Instance.SystemModeChanged += On_SystemModeChanged;
+        }
+
+        #endregion
+
+        #region Functionality
+
+        private void Configure
+
+        #endregion
+
+        #region SystemModeChanged
+
+        private void On_SystemModeChanged(object sender, SystemModeChanged_EventArgs e)
+        {
+            if (e.SelectedMode == SystemModeCommands.SourceSetup)
+            {
+                _IsSourceSetupPageSelected = true;
+                _TheDevice.SendCommandRequest("SS");
+            }
+            else
+                _IsSourceSetupPageSelected = false;
+        }
+
+        #endregion
     }
 }
