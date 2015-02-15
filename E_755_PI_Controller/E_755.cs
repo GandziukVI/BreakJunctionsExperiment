7d66775cc69af5dfbb8186f0c31f942649eae148
GandziukVI <gandziuk2009@gmail.com>
2015-02-15 20:40:48 +0100

    Motion controller E_755 Programming started

---
+++ E_755_PI_Controller/E_755.cs
@@ -0,0 +1,56 @@
+﻿using Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_755_PI_Controller
+{
+    public class E_755
+    {
+        #region E_755 settings
+
+        private IExperimentalDevice _TheDevice;
+
+        #endregion
+
+        #region Constructor
+
+        public E_755(ref IExperimentalDevice __TheDevice)
+        {
+            _TheDevice = __TheDevice;
+        }
+
+        #endregion
+
+        #region Functionality
+
+        public string RequestMotionStatus()
+        {
+            return _TheDevice.RequestQuery("#5");
+        }
+
+        public string GetReadyStatus()
+        {
+            return _TheDevice.RequestQuery("#7");
+        }
+
+        public void StopAllAxes()
+        {
+            _TheDevice.SendCommandRequest("#24");
+        }
+
+        public string GetID()
+        {
+            return _TheDevice.RequestQuery("*IDN?");
+        }
+
+        public void AutoPiezoGainCalibration()
+        {
+
+        }
+
+        #endregion
+    }
+}
