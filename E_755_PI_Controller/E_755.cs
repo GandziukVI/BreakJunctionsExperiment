fadd1e8ccf4abbab36301497eca87336c680dfbe
GandziukVI <gandziuk2009@gmail.com>
2015-02-17 17:23:38 +0100

    Motor motion implementation in process

--- E_755_PI_Controller/E_755.cs
+++ E_755_PI_Controller/E_755.cs
@@ -46,9 +46,110 @@ namespace E_755_PI_Controller
             return _TheDevice.RequestQuery("*IDN?");
         }
 
-        public void AutoPiezoGainCalibration()
+        public void AutoPiezoGainCalibration(GainIDs __GainID)
         {
+            var command = String.Format("APG [{{{0}}}]", (int)__GainID);
+            _TheDevice.SendCommandRequest(command);
+        }
 
+        public APG_State ReadAutoPiezoGainCalibrationState(GainIDs __GainID)
+        {
+            var query = String.Format("APG? [{{{0}}}]", (int)__GainID);
+            var responce = 0;
+
+            int.TryParse(_TheDevice.RequestQuery(query), out responce);
+            return (APG_State)responce;
+        }
+
+        public void SetBaudRate(BaudRates __BaudRate)
+        {
+            var command = String.Format("BDR {0}", (int)__BaudRate);
+            _TheDevice.SendCommandRequest(command);
+        }
+
+        public BaudRates GetBaudRate()
+        {
+            var query = "BDR?";
+            var responce = 57600;
+
+            int.TryParse(_TheDevice.RequestQuery(query), out responce);
+            return (BaudRates)responce;
+        }
+
+        public void ChangeActiveCommandLevel(CommandLevels __CommandLevel)
+        {
+            var command = "";
+            switch (__CommandLevel)
+            {
+                case CommandLevels.Default:
+                    {
+                        command = String.Format("CCL {0}", (int)__CommandLevel);
+                        _TheDevice.SendCommandRequest(command);
+                    } break;
+                case CommandLevels.Advanced:
+                    {
+                        command = String.Format("CCL {0} [{1}]", (int)__CommandLevel, "advanced");
+                        _TheDevice.SendCommandRequest(command);
+                    } break;
+                case CommandLevels.Highest:
+                    throw new Exception("Nott allover for usual user!");
+                default:
+                    {
+                        command = String.Format("CCL {0}", (int)__CommandLevel);
+                        _TheDevice.SendCommandRequest(command);
+                    } break;
+            }
+        }
+
+        public CommandLevels GetActiveCommandLevel()
+        {
+            var query = "CCL?";
+            var responce = 0;
+
+            int.TryParse(_TheDevice.RequestQuery(query), out responce);
+            return (CommandLevels)responce;
+        }
+
+        public void ClearAxisStatus(AxisIdentifier __AxisID)
+        {
+            var command = String.Format("CLR [{{{0}}}]", (int)__AxisID);
+            _TheDevice.SendCommandRequest(command);
+        }
+
+        public string GetAssignmentOfStagesToAxes(AxisIdentifier __AxesID)
+        {
+            var query = String.Format("CST? {{{0}}}", (int)__AxesID);
+            return _TheDevice.RequestQuery(query);
+        }
+
+        public string GetCommandSyntaxVersion()
+        {
+            var query = "CSV?";
+            return _TheDevice.RequestQuery(query);
+        }
+
+        public void SetDaisyChainConfiguration(ProtocolType __ProtocolType, ProtocolOptions __ProtocolOptions)
+        {
+            var command = String.Format("DCC {0} {1}", (int)__ProtocolType, (int)__ProtocolOptions);
+            _TheDevice.SendCommandRequest(command);
+        }
+
+        public string GetDaisyChainConfiguration(ProtocolType __ProtocolType)
+        {
+            var query = String.Format("DCC [{0}]", (int)__ProtocolType);
+            return _TheDevice.RequestQuery(query);
+        }
+
+        public void DefineHome(AxisIdentifier __AxisID)
+        {
+            var command = String.Format("DFH [{{{0}}}]", (int)__AxisID);
+            _TheDevice.SendCommandRequest(command);
+        }
+
+        public string GetHomePositions(AxisIdentifier __AxisID)
+        {
+            var query = String.Format("DFH? [{{{0}}}]", (int)__AxisID);
+            return _TheDevice.RequestQuery(query);
         }
 
         #endregion
--- KeithleyTest/Form1.cs
+++ KeithleyTest/Form1.cs
@@ -12,6 +12,7 @@ using Devices;
 using Devices.SMU;
 using Keithley_4200;
 using Keithley_4200.Pages;
+using E_755_PI_Controller;
 
 namespace SMU.KeithleyTest
 {
@@ -22,11 +23,14 @@ namespace SMU.KeithleyTest
             InitializeComponent();
 
             IExperimentalDevice d = new GPIB_Device(17, 0, 0);
-            var a = new Keithley_4200_SMU(ref d, SMUs.SMU2);
-            a.CurrentLimit = 0.001;
-            var b = a.MeasureResistance(0.01, 2, 0, SourceMode.Voltage);
-            var c = new ChannelDefinitionPage(ref d);
-            c.DisableChannel(SMUs.SMU2);
+            //var a = new Keithley_4200_SMU(ref d, SMUs.SMU2);
+            //a.CurrentLimit = 0.001;
+            //var b = a.MeasureResistance(0.01, 2, 0, SourceMode.Voltage);
+            //var c = new ChannelDefinitionPage(ref d);
+            //c.DisableChannel(SMUs.SMU2);
+
+            var e = new E_755(ref d);
+            e.AutoPiezoGainCalibration(GainIDs.ChannelID_02);
         }
     }
 }
