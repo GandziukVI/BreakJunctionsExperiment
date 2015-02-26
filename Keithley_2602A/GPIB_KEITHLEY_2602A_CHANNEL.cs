abca5e4fb91956d4ace61150fccca18ebb689f5b
GandziukVI <gandziuk2009@gmail.com>
2015-02-26 19:42:47 +0100

    Keithley using IVI driver

--- Keithley_2602A/GPIB_KEITHLEY_2602A_CHANNEL.cs
+++ Keithley_2602A/GPIB_KEITHLEY_2602A_CHANNEL.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Globalization;
 
+using Ivi.Driver.Interop;
+using Keithley.Ke26XXA.Interop;
+
 using Devices.SMU;
 
 namespace SMU.KEITHLEY_2602A
 {
-    public class GPIB_KEITHLEY_2602A_CHANNEL : GPIB_KEITHLEY_2602A, I_SMU
+    public class GPIB_KEITHLEY_2602A_CHANNEL : I_SMU
     {
         private NumberStyles style;
         private CultureInfo culture;
@@ -20,30 +23,106 @@ namespace SMU.KEITHLEY_2602A
             set { _SelectedChannel = value; }
         }
 
+        private IKe26XXA _Driver;
+
         public GPIB_KEITHLEY_2602A_CHANNEL(byte _PrimaryAddress, byte _SecondaryAddress, byte _BoardNumber, Channels _Channel)
-            : base(_PrimaryAddress, _SecondaryAddress, _BoardNumber)
         {
+            _Driver = new Ke26XXA();
+            //Initialization
+            if (_Driver.Initialized)
+                _Driver.Close();
+            var options = "QueryInstrStatus=True";
+            _Driver.Initialize(String.Format("GPIB::{0}::INSTR", _PrimaryAddress), true, true, options);
+
             style = NumberStyles.Float;
             culture = CultureInfo.CreateSpecificCulture("en-US");
 
             _SelectedChannel = _Channel;
         }
 
+        public bool InitDevice()
+        {
+
+            try
+            {
+                _Driver.System.DirectIO.WriteString("beeper.enable = 1 ");
+                return true;
+            }
+            catch { return false; }
+        }
+
+        private double _FastestSpeed = 0.001;
+        private double _LowestSpeed = 25.0;
+        public void SetSpeed(double Speed, Channels SelectedChannel)
+        {
+            var Command = "smu{0}.measure.nplc = {1} ";
+            var _Speed = Speed.ToString().Replace(',', '.');
+
+            if (Speed < _FastestSpeed) Speed = _FastestSpeed;
+            else if (Speed > _LowestSpeed) Speed = _LowestSpeed;
+
+            switch (SelectedChannel)
+            {
+                case Channels.ChannelA:
+                    {
+                        _Driver.System.DirectIO.WriteString(String.Format(Command, "a", _Speed));
+                    } break;
+                case Channels.ChannelB:
+                    {
+                        _Driver.System.DirectIO.WriteString(String.Format(Command, "b", _Speed));
+                    } break;
+                default:
+                    break;
+            }
+        }
+
         public void SwitchON()
         {
-            SwitchChannelState(_SelectedChannel, Channel_Status.Channel_ON);
+            _Driver.Utility.Reset();
+            switch (_SelectedChannel)
+            {
+                case Channels.ChannelA:
+                    {
+                        _Driver.Source.set_OutputEnabled("A", true);
+                    } break;
+                case Channels.ChannelB:
+                    {
+                        _Driver.Source.set_OutputEnabled("B", true);
+                    } break;
+            }
         }
 
         public void SwitchOFF()
         {
-            SwitchChannelState(_SelectedChannel, Channel_Status.Channel_OFF);
+            _Driver.Utility.Reset();
+            switch (_SelectedChannel)
+            {
+                case Channels.ChannelA:
+                    {
+                        _Driver.Source.set_OutputEnabled("A", false);
+                    } break;
+                case Channels.ChannelB:
+                    {
+                        _Driver.Source.set_OutputEnabled("B", false);
+                    } break;
+            }
         }
 
         public bool SetVoltageLimit(double Value)
         {
             try
             {
-                SetSourceLimit(Value, LimitMode.Voltage, _SelectedChannel);
+                switch (_SelectedChannel)
+                {
+                    case Channels.ChannelA:
+                        {
+                            _Driver.Source.Voltage.set_Limit("A", Value);
+                        } break;
+                    case Channels.ChannelB:
+                        {
+                            _Driver.Source.Voltage.set_Limit("B", Value);
+                        } break;
+                }
                 return true;
             }
             catch { return false; }
@@ -53,7 +132,17 @@ namespace SMU.KEITHLEY_2602A
         {
             try
             {
-                SetSourceLimit(Value, LimitMode.Current, _SelectedChannel);
+                switch (_SelectedChannel)
+                {
+                    case Channels.ChannelA:
+                        {
+                            _Driver.Source.Current.set_Limit("A", Value);
+                        } break;
+                    case Channels.ChannelB:
+                        {
+                            _Driver.Source.Current.set_Limit("B", Value);
+                        } break;
+                }
                 return true;
             }
             catch { return false; }
@@ -63,7 +152,17 @@ namespace SMU.KEITHLEY_2602A
         {
             try
             {
-                SetValueToChannel(Value, SourceMode.Voltage, _SelectedChannel);
+                switch (_SelectedChannel)
+                {
+                    case Channels.ChannelA:
+                        {
+                            _Driver.Source.Voltage.set_Level("A", Value);
+                        } break;
+                    case Channels.ChannelB:
+                        {
+                            _Driver.Source.Voltage.set_Level("B", Value);
+                        } break;
+                }
                 return true;
             }
             catch { return false; }
@@ -73,7 +172,17 @@ namespace SMU.KEITHLEY_2602A
         {
             try
             {
-                SetValueToChannel(Value, SourceMode.Current, _SelectedChannel);
+                switch (_SelectedChannel)
+                {
+                    case Channels.ChannelA:
+                        {
+                            _Driver.Source.Current.set_Level("A", Value);
+                        } break;
+                    case Channels.ChannelB:
+                        {
+                            _Driver.Source.Current.set_Level("B", Value);
+                        } break;
+                }
                 return true;
             }
             catch { return false; }
@@ -81,80 +190,103 @@ namespace SMU.KEITHLEY_2602A
 
         public double MeasureVoltage(int NumberOfAverages, double TimeDelay)
         {
-            double MeasuredVoltage;
+            var bufName = "MyBuf";
+            switch (_SelectedChannel)
+            {
+                case Channels.ChannelA:
+                    {
+                        _Driver.Measurement.Voltage.set_AutoRangeEnabled("A", true);
+                        _Driver.Measurement.set_Count("A", NumberOfAverages);
+                        _Driver.Measurement.set_Delay("A", TimeDelay);
+                        _Driver.Measurement.Buffer.Create("A", bufName, NumberOfAverages);
+                        _Driver.Measurement.Voltage.MeasureMultiple("A", bufName);
+                    } break;
+                case Channels.ChannelB:
+                    {
+                        _Driver.Measurement.Voltage.set_AutoRangeEnabled("B", true);
+                        _Driver.Measurement.set_Count("B", NumberOfAverages);
+                        _Driver.Measurement.set_Delay("B", TimeDelay);
+                        _Driver.Measurement.Buffer.Create("B", bufName, NumberOfAverages);
+                        _Driver.Measurement.Voltage.MeasureMultiple("B", bufName);
+                    } break;
+            }
 
-            var MeasuredVoltageString = MeasureIV_ValueInChannel(_SelectedChannel, MeasureMode.Voltage, NumberOfAverages, TimeDelay).TrimEnd('\n');
-            var isSucceed = double.TryParse(MeasuredVoltageString, style, culture, out MeasuredVoltage);
-
-            if (isSucceed)
-                return MeasuredVoltage;
-            else return double.NaN;
+            return _Driver.Measurement.Buffer.MeasureData.GetAllReadings(bufName).Average();
         }
 
         public double MeasureCurrent(int NumberOfAverages, double TimeDelay)
         {
-            double MeasuredCurrent;
+            var bufName = "MyBuf";
+            switch (_SelectedChannel)
+            {
+                case Channels.ChannelA:
+                    {
+                        _Driver.Measurement.Current.set_AutoRangeEnabled("A", true);
+                        _Driver.Measurement.set_Count("A", NumberOfAverages);
+                        _Driver.Measurement.set_Delay("A", TimeDelay);
+                        _Driver.Measurement.Buffer.Create("A", bufName, NumberOfAverages);
+                        _Driver.Measurement.Current.MeasureMultiple("A", bufName);
+                    } break;
+                case Channels.ChannelB:
+                    {
+                        _Driver.Measurement.Current.set_AutoRangeEnabled("B", true);
+                        _Driver.Measurement.set_Count("B", NumberOfAverages);
+                        _Driver.Measurement.set_Delay("B", TimeDelay);
+                        _Driver.Measurement.Buffer.Create("B", bufName, NumberOfAverages);
+                        _Driver.Measurement.Current.MeasureMultiple("B", bufName);
+                    } break;
+            }
 
-            var MeasuredCurrentString = MeasureIV_ValueInChannel(_SelectedChannel, MeasureMode.Current, NumberOfAverages, TimeDelay).TrimEnd('\n');
-            var isSucceed = double.TryParse(MeasuredCurrentString, style, culture, out MeasuredCurrent);
-
-            if (isSucceed)
-                return MeasuredCurrent;
-            else return double.NaN;
+            return _Driver.Measurement.Buffer.MeasureData.GetAllReadings(bufName).Average();
         }
 
 
         public double MeasureResistance(double valueThroughTheStructure, int NumberOfAverages, double TimeDelay, SourceMode sourceMode)
         {
-            double measuredResistance;
-            SourceMode _sourceMode = SourceMode.Voltage;
-
-            switch (sourceMode)
+            var bufName = "MyBuf";
+            switch (_SelectedChannel)
             {
-                case SourceMode.Voltage:
+                case Channels.ChannelA:
                     {
-                        _sourceMode = SourceMode.Voltage;
+                        _Driver.Measurement.set_Count("A", NumberOfAverages);
+                        _Driver.Measurement.set_Delay("A", TimeDelay);
+                        _Driver.Measurement.Buffer.Create("A", bufName, NumberOfAverages);
+                        _Driver.Measurement.Resistance.MeasureMultiple("A", bufName);
                     } break;
-                case SourceMode.Current:
-                    { 
-                        _sourceMode = SourceMode.Current;
+                case Channels.ChannelB:
+                    {
+                        _Driver.Measurement.set_Count("B", NumberOfAverages);
+                        _Driver.Measurement.set_Delay("B", TimeDelay);
+                        _Driver.Measurement.Buffer.Create("B", bufName, NumberOfAverages);
+                        _Driver.Measurement.Resistance.MeasureMultiple("B", bufName);
                     } break;
-                default:
-                    break;
             }
-            
-            var measuredRessitanceString = MeasureResistanceOrPowerValueInChannel(_SelectedChannel, _sourceMode, MeasureMode.Resistance, valueThroughTheStructure, NumberOfAverages, TimeDelay).TrimEnd('\n');
-            var isSucceed = double.TryParse(measuredRessitanceString, style, culture, out measuredResistance);
 
-            if (isSucceed)
-                return measuredResistance;
-            else return double.NaN;
+            return _Driver.Measurement.Buffer.MeasureData.GetAllReadings(bufName).Average();
         }
 
         public double MeasurePower(double valueThroughTheStructure, int NumberOfAverages, double TimeDelay, SourceMode sourceMode)
         {
-            double measuredPower;
-            SourceMode _sourceMode = SourceMode.Voltage;
-
-            switch (sourceMode)
+            var bufName = "MyBuf";
+            switch (_SelectedChannel)
             {
-                case SourceMode.Voltage:
+                case Channels.ChannelA:
                     {
-                        _sourceMode = SourceMode.Voltage;
+                        _Driver.Measurement.set_Count("A", NumberOfAverages);
+                        _Driver.Measurement.set_Delay("A", TimeDelay);
+                        _Driver.Measurement.Buffer.Create("A", bufName, NumberOfAverages);
+                        _Driver.Measurement.Power.MeasureMultiple("A", bufName);
                     } break;
-                case SourceMode.Current:
+                case Channels.ChannelB:
                     {
-                        _sourceMode = SourceMode.Current;
+                        _Driver.Measurement.set_Count("B", NumberOfAverages);
+                        _Driver.Measurement.set_Delay("B", TimeDelay);
+                        _Driver.Measurement.Buffer.Create("B", bufName, NumberOfAverages);
+                        _Driver.Measurement.Power.MeasureMultiple("B", bufName);
                     } break;
-                default:
-                    break;
             }
-            var measuredPowerString = MeasureResistanceOrPowerValueInChannel(_SelectedChannel, _sourceMode, MeasureMode.Power, valueThroughTheStructure, NumberOfAverages, TimeDelay).TrimEnd('\n');
-            var isSucceed = double.TryParse(measuredPowerString, style, culture, out measuredPower);
 
-            if (isSucceed)
-                return measuredPower;
-            else return double.NaN;
+            return _Driver.Measurement.Buffer.MeasureData.GetAllReadings(bufName).Average();
         }
     }
 }
